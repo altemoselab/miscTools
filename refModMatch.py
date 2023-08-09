baedfe35415870323028b7fa212a9bba79bef9e4
Danilo Dubocanin <danilodubocanin@DN52ega1.SUNet>
2023-08-09 10:02:01 -0700

    v1 working

--- refModMatch.py
+++ refModMatch.py
@@ -7,6 +7,7 @@ import gzip
 import subprocess
 import shlex
 import tqdm
+from array import array
 
 # remove modifications that are not reference matched
 # requires MD tag
@@ -28,7 +29,7 @@ for read in tqdm.tqdm(bam.fetch()):
 		aligned_pairs = np.array(read.get_aligned_pairs(with_seq=True,matches_only=False))
 
 		# find all indel/padding positions
-		none_idx = aligned_pairs[:,2]==None
+		none_idx = aligned_pairs[:,2] == None
 
 		# replace those with lowercase string for vectorized operation
 		aligned_pairs[:,2][none_idx] = "n"
@@ -57,11 +58,18 @@ for read in tqdm.tqdm(bam.fetch()):
 		
 		original_mm = original_mm.split(';')[:-1]
 
+		new_ml = []
+		ml_counter = 0
+		all_mm = []
+		mod_type_tracker = []
 		for mm in original_mm:
+			
 			split_mm = mm.split(',')
 			base_mod = split_mm[0]
 			forward_mod_positions = np.array(forward_mods.get((base_mod[0],0,base_mod[-1])))
 			mm_positions = np.array(split_mm[1:]).astype(int)
+			mod_type_tracker.append(base_mod)
+
 			if forward_mod_positions.size > 1 and len(mm_positions) > 0:
 
 				forward_mod_positions = forward_mod_positions[:,0]
@@ -86,7 +94,6 @@ for read in tqdm.tqdm(bam.fetch()):
 					tmp_mm = mm_positions.copy()
 
 					ranges_idx = range(len(idx_remove_start))
-					print(forward_mod_to_remove)
 					for i in ranges_idx:
 
 						start, stop = int(idx_remove_start[i]), int(idx_remove_stop[i])
@@ -94,10 +101,47 @@ for read in tqdm.tqdm(bam.fetch()):
 
 						tmp_mm[stop] = mm_positions[stop] + np.sum(mm_positions[start:stop] + 1)
 					new_mm = tmp_mm[forward_mod_to_remove==False]
+					if len(new_mm) == 0:
+						output_bam.write(read)
+						continue
+					all_mm.append(new_mm)
+					
 
-					print(mm_positions)
-					print(new_mm)
-					print('---')
+					## new_MM is good and represents the new mms we want to use
+					# now we need to index the ML tag using the forward_mod_to_remove 
+					# and keep count of how many positions we consume 
+					# to then filter for the next modification type
+
+					tmp_ml = np.array(original_ml[ml_counter:ml_counter + len(forward_mod_to_remove)])
+					new_ml.append(tmp_ml[forward_mod_to_remove==False])
+					
+					ml_counter =+ len(forward_mod_to_remove)
+
+
+		if len(new_ml) < 2:
+			output_bam.write(read)
+			continue
+		new_ml = np.concatenate(new_ml)
+
+		output_ml = list(new_ml.astype(int))
+		read.set_tag('ML' ,array('B', output_ml))
+		
+		output_mm_list = []
+		
+		for mod_type, mod_mm_positions in zip(mod_type_tracker, all_mm):
+			output_mm_list.append(mod_type + "," + ','.join(mod_mm_positions.astype(str)))
+
+
+		output_mm_string = ';'.join(output_mm_list) + ";"
+
+		read.set_tag('MM', output_mm_string, "Z")
+
+
+					## new_MM is good and represents the new mms we want to use
+					# now we need to index the ML tag using the forward_mod_to_remove 
+					# and keep count of how many positions we consume 
+					# to then filter for the next modification type
+
 
 
 
@@ -162,35 +206,6 @@ for read in tqdm.tqdm(bam.fetch()):
 	output_bam.write(read)
 
 
-
-
-
-
-	# for read in tqdm(infile.fetch()):
-	
-	# if read.has_tag('MM'):
-		
-	# 	for_seq = read.get_forward_sequence().upper()
-
-	# 	T_count = for_seq.count("T")
-
-	# 	mod_read = read.modified_bases_forward
-
-	# 	analog_count = 0
-
-	# 	for mod in mod_read:
-	# 		if mod[0] == "T":
-	# 			if mod[2] == 'e' or mod[2] == 'b':
-
-	# 				mm_ml = np.vstack(mod_read[mod]).T
-
-	# 				analog_count += np.sum(mm_ml[1] > prob_cutoff)
-
-	# 	print(analog_count / T_count)
-
-
-
-	# 	# outfile.write(read)
 
 
 
